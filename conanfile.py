742478d51a3aea3851782c3b0f5c45aaf493e69b
Carlos Zoido <mrgalleta@gmail.com>
2026-06-09 13:57:10 +0200

    Port build system to conan-py-build

---
+++ conanfile.py
@@ -0,0 +1,24 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+
+
+class ConfluentKafkaPythonConan(ConanFile):
+    name = "confluent-kafka"
+    version = "2.14.2"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires("librdkafka/[>=2.0 <3]")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
